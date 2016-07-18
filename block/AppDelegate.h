2b49f15c86e45676c1a76e4a26e635ab0260a470
YuZhongqi <403998424@qq.com>
2016-07-18 22:18:02 +0800

    Initial Commit

---
+++ block/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  block
+//
+//  Created by YuZhongqi on 16/7/18.
+//  Copyright © 2016年 YuZhongqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ block/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  block
+//
+//  Created by YuZhongqi on 16/7/18.
+//  Copyright © 2016年 YuZhongqi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
